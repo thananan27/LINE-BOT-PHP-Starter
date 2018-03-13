833948d3e4594314a53d96e92c815405be34d597
thananan27 <thananan27@gmail.com>
2018-03-13 18:33:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -5,6 +5,13 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
+if (!is_null($events['ESP'])) {
+	
+	send_LINE($events['ESP']);
+		
+	echo "OK";
+	}
+// Validate parsed JSON data
 if (!is_null($events['events'])) {	
 // Loop through each event	
 foreach ($events['events'] as $event) {		
